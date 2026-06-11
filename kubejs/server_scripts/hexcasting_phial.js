afa7028e01621f2d5735e82b8e884fa9d6bf4a5c
Teal Wolf 25 <teal@tw25.net>
2026-06-11 18:10:06 -0400

    phial fix pt 2

--- kubejs/server_scripts/hexcasting_phial.js
+++ kubejs/server_scripts/hexcasting_phial.js
@@ -3,17 +3,18 @@ ServerEvents.recipes(event => {
 		let phials = grid.findAll(Item.of("hexcasting:battery").ignoreNBT())
 		let media = 0
 		let maxMedia = 0
-		let maxMedias = []
+		let maxMediaCurrent = 0
 		let recharge = 0
 		let nbt
 		phials.forEach(phial => {
 			media += phial.nbt["hexcasting:media"]
 			recharge += (phial.nbt["hexcasting:start_media"] - phial.nbt["hexcasting:media"])*0.1
-			maxMedias.push(phial.nbt["hexcasting:start_media"])
+			if (maxMediaCurrent < phial.nbt["hexcasting:start_media"]) maxMediaCurrent = phial.nbt["hexcasting:start_media"]
 			if (phial.nbt != null) nbt = phial.nbt
     });
 
-		maxMedia = Math.round(Math.max(...maxMedias) + recharge)
+    maxMedia = Math.round(maxMediaCurrent + recharge)
+		if (media > maxMedia) media = maxMedia
 		if (nbt == undefined && media > 128) return itemstack
 		nbt["hexcasting:media"] = media
 		nbt["hexcasting:start_media"] = maxMedia
